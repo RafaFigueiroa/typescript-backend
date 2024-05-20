d4f781526b7b2c76f73cdfcadfb6dc49b88f979e
RafaFigueiroa <rafatinsfig@gmail.com>
2024-05-20 00:31:33 -0300

    melhorias no CRUD de pet, implementacao de um tipo e enum

--- src/routes/petRouter.ts
+++ src/routes/petRouter.ts
@@ -1,10 +1,13 @@
 import express from "express";
-import PetController from "../controller/petController";
+import PetController from "../controller/petController"
 
 const router = express.Router();
 
 const petController = new PetController();
 
 router.post("/", petController.criaPet);
+router.get("/", petController.listaPets);
+router.put("/:id", petController.atualizaPets);
+router.delete("/:id", petController.deletaPet);
 
 export default router;
